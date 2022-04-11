6291341aa1382afd175483c8d6e5d2aced8e8e7a
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-11 19:26:06 +0530

    Calculate Wages of Employee till condition of total working hrs or days is reached

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.employeewagecomp;
 
 /**
- * Program to Calculate Wages of Employee for a Month assuming 20 Working Days
- * in a Month
+ * Program to Calculate Wages till a condition of total working hours or days is
+ * reached for a month - Assume 100 hours and 20 days
  * 
  * @author : Snehal Patil
  *
@@ -12,20 +12,20 @@ public class EmployeeWageComputation {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_WAGE_PER_HR = 20;
-	public static final int NO_WORKING_DAYS = 20;
+	public static final int MAX_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
-
-		// Variables
-		int empHrs = 0, empWage = 0, TotalEmpWage = 0;
+		// Variable
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 1;
 
 		/*
 		 * Using Random function taking input either 0 or 1 or 2. Using Switch cases
 		 * (0)_Default = Employee absent ,1 = full time and 0 = Part time . calculating
-		 * monthly wage assuming total working days in a month are 20.
+		 * monthly wage assuming total working days or total working hours reached are
+		 * 20 days and 100 hrs.
 		 */
-		for (int day = 1; day <= NO_WORKING_DAYS; day++) {
+		while ((totalEmpHrs <= MAX_HRS_IN_MONTH) && (totalWorkingDays <= MAX_WORKING_DAYS)) {
 			int Emp_Check = (int) Math.floor(Math.random() * 10) % 3;
 
 			switch (Emp_Check) {
@@ -43,13 +43,14 @@ public class EmployeeWageComputation {
 				System.out.println("Employee is Absent");
 				empHrs = 0;
 			}
-			empWage = empHrs * EMP_WAGE_PER_HR;
-			TotalEmpWage += empWage;
-			System.out.println("Employee Wage is : " + empWage);
+			totalEmpHrs += empHrs;
+			totalWorkingDays++;
+			System.out.println("Employee Wage is : " + empHrs);
 			System.out.println();
 		}
-		// Monthly wage
-		System.out.println("Monthly Wage of employee is : " + TotalEmpWage);
-		System.out.println();
+		// Calculating Monthly wage
+		int totalMonthlyWage = totalEmpHrs * EMP_WAGE_PER_HR;
+		System.out.println("Employee worked " + totalEmpHrs + " Hrs in " + (totalWorkingDays - 1) + " days.");
+		System.out.println("Monthly Wage of employee is : " + totalMonthlyWage);
 	}
 }
