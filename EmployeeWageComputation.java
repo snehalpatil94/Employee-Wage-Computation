af6df1c0fc23a870061d8153b454facce6a9b84c
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-11 18:10:39 +0530

    Program to Calculate Employee Wage Using Switch Cases.

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,38 +1,44 @@
 package com.bridgelabz.employeewagecomp;
 
 /**
- * Program to Check Whether employee is full time or part time.Calculate wage..
+ * Program to Calculate Employee Wage Using Switch Cases.
  * 
  * @author : Snehal Patil
  *
  */
 public class EmployeeWageComputation {
+	// Constants
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_WAGE_PER_HR = 20;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
 
-		// Constants
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_WAGE_PER_HR = 20;
-
-		// Variable
+		// Variables
 		int empHrs = 0;
 		int empWage = 0;
 
 		/*
-		 * Using Random function taking input either 0 or 1 or 2. calculating wage
-		 * accordingly
+		 * Using Random function taking input either 0 or 1 or 2. Using Switch cases
+		 * (0)_Default = Employee absent ,1 = full time and 0 = Part time . calculating
+		 * wage accordingly
 		 */
-		int Emp_Check = (int) (Math.floor(Math.random() * 10) % 3);
+		int Emp_Check = (int) Math.floor(Math.random() * 10) % 3;
 		System.out.println("Random value for emp Attendence check is: " + Emp_Check);
 
-		if (Emp_Check == IS_FULL_TIME) {
+		switch (Emp_Check) {
+		case IS_FULL_TIME:
 			System.out.println("Employee Present : Full Time");
 			empHrs = 8;
-		} else if ((Emp_Check == IS_PART_TIME)) {
+			break;
+
+		case IS_PART_TIME:
 			System.out.println("Employee is Present : Part Time");
 			empHrs = 4;
-		} else {
+			break;
+
+		default:
 			System.out.println("Employee is Absent");
 			empHrs = 0;
 		}
