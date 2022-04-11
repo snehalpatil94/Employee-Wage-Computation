29b9018b527f0908ba7dc65628cc8ddb08171b9e
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-11 19:15:02 +0530

    Calculate Wages of Employee for a Month assuming 20 Working Days

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.employeewagecomp;
 
 /**
- * Program to Calculate Employee Wage Using Switch Cases.
+ * Program to Calculate Wages of Employee for a Month assuming 20 Working Days
+ * in a Month
  * 
  * @author : Snehal Patil
  *
@@ -11,38 +12,44 @@ public class EmployeeWageComputation {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_WAGE_PER_HR = 20;
+	public static final int NO_WORKING_DAYS = 20;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
 
 		// Variables
-		int empHrs = 0;
-		int empWage = 0;
+		int empHrs = 0, empWage = 0, TotalEmpWage = 0;
 
 		/*
 		 * Using Random function taking input either 0 or 1 or 2. Using Switch cases
 		 * (0)_Default = Employee absent ,1 = full time and 0 = Part time . calculating
-		 * wage accordingly
+		 * monthly wage assuming total working days in a month are 20.
 		 */
-		int Emp_Check = (int) Math.floor(Math.random() * 10) % 3;
-		System.out.println("Random value for emp Attendence check is: " + Emp_Check);
+		for (int day = 1; day <= NO_WORKING_DAYS; day++) {
+			int Emp_Check = (int) Math.floor(Math.random() * 10) % 3;
 
-		switch (Emp_Check) {
-		case IS_FULL_TIME:
-			System.out.println("Employee Present : Full Time");
-			empHrs = 8;
-			break;
+			switch (Emp_Check) {
+			case IS_FULL_TIME:
+				System.out.println("Employee Present : Full Time");
+				empHrs = 8;
+				break;
 
-		case IS_PART_TIME:
-			System.out.println("Employee is Present : Part Time");
-			empHrs = 4;
-			break;
+			case IS_PART_TIME:
+				System.out.println("Employee is Present : Part Time");
+				empHrs = 4;
+				break;
 
-		default:
-			System.out.println("Employee is Absent");
-			empHrs = 0;
+			default:
+				System.out.println("Employee is Absent");
+				empHrs = 0;
+			}
+			empWage = empHrs * EMP_WAGE_PER_HR;
+			TotalEmpWage += empWage;
+			System.out.println("Employee Wage is : " + empWage);
+			System.out.println();
 		}
-		empWage = empHrs * EMP_WAGE_PER_HR;
-		System.out.println("Employee Wage is : " + empWage);
+		// Monthly wage
+		System.out.println("Monthly Wage of employee is : " + TotalEmpWage);
+		System.out.println();
 	}
 }
