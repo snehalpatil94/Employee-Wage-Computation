0a70e160dc50f74df0b5e3c61c1410caa294de16
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-25 22:37:20 +0530

    Employee wages for multiple company using Array List

--- ICompanyEmpWage.java
+++
@@ -1,6 +0,0 @@
-package com.bridgelabz.employeewagecomp;
-
-public interface ICompanyEmpWage {
-	public void addComapanyEmpWage(String company, int empWagePerHr, int maxWorkingDays, int maxHrPerMonth);
-	public void empWageCalculation();
-}
